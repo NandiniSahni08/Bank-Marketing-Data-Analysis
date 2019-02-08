2f18464b8fb1884ffd8a544d6bb37e72e8c4543a
NandiniSahni08 <47262156+NandiniSahni08@users.noreply.github.com>
2019-02-08 13:41:01 -0500

    Add files via upload

---
+++ Bank_Marketing_DataAnalysis.R
@@ -0,0 +1,12 @@
+bank <- read.csv("nandinisahni/Documents/Study/semester2/ML&Stats/bank/bank-full.csv")
+bank <- bank.full
+bank_add <- bank.additional.full
+
+head(bank)
+head(bank_add)
+dim(bank)
+dim(bank_add)
+str(bank)
+str(bank_add)
+summary(bank)
+summary(bank_add)
